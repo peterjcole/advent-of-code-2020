026287d28cd06ba90f083b42554524814e94b23f
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2020-12-08 17:07:14 +0000

    Completed day 1 part 2

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # advent-of-code-2020
-My solutions to Advent of Code 2020 https://adventofcode.com/2020
+> My solutions to Advent of Code 2020 
+> https://adventofcode.com/2020
+
+
+To run:
+```bash
+cd day1
+node day1.js
+```
--- day1/day1.js
+++ day1/day1.js
@@ -1,20 +1,41 @@
 const fs = require("fs")
 
-const day1 = (function() {
-
+const day1 = (function () {
     const expenses = fs.readFileSync('./expense-report.txt')
-    .toString('utf-8')
-    .split('\n')
-    .map(expenseString => parseInt(expenseString))
+        .toString('utf-8')
+        .split('\n')
+        .map(expenseString => parseInt(expenseString))
 
     function part1() {
         const product = expenses.reduce((foundProduct, firstExpense) => {
             return foundProduct || expenses.find((secondExpense) => secondExpense + firstExpense === 2020) * firstExpense
         }, undefined)
 
-        console.log(`Part 1 - product of entries which sum to 2020: ${product}`)
+        console.log(`Part 1 - product of 2 entries which sum to 2020: ${product}`)
+    }
+
+
+    function part2() {
+        let product
+
+        expenses.forEach((currentFirst, firstIndex) => {
+            expenses.forEach((currentSecond, secondIndex) => {
+                expenses.forEach((currentThird, thirdIndex) => {
+                    if (firstIndex !== secondIndex 
+                        && secondIndex !== thirdIndex 
+                        && firstIndex !== thirdIndex) {
+                        if (currentFirst + currentSecond + currentThird === 2020) {
+                            product = currentFirst * currentSecond * currentThird
+                        }
+                    }
+                })
+            })
+        })
+
+        console.log(`Part 2 - product of 3 entries which sum to 2020: ${product}`)
     }
 
     part1()
-    
-}());
+    part2()
+
+}())
